96b63544461b2ed9f5a1353158b4eff7aded7ed8
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2024-09-19 12:26:04 -0700

    Adds tailwind to style cms

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./frontend/src/**/*.{jsx, js}",
+    "./frontend/src/**/*.css",
+    "./layouts/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
