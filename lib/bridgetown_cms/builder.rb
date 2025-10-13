ada6a0fe19757e8086b08aa4048eef565c157d5f
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2025-10-12 19:55:52 -0700

    Add builder and script

--- lib/bridgetown_cms/builder.rb
+++ lib/bridgetown_cms/builder.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
+require "bridgetown_cms/admin_routes"
+
 module BridgetownCms
   class Builder < Bridgetown::Builder
     def build
       liquid_tag "bridgetown_cms" do
         "This plugin works!"
+      end
+
+      # Log that the cms has been loaded
+      hook :site, :after_init do
+        Bridgetown.logger.info "🎨 Bridgetown CMS loaded!"
+        Bridgetown.logger.info "📝 Run 'bin/bridgetown apply https://github.com/adrianvalenz/bridgetown_cms' to set up admin routes"
       end
     end
   end
